f9fc9d6449e2f2a3f66f9b7e6d27123cdb6d2c28
ghostrich <wiebke.hurrelmann@gmx.de>
2020-10-03 17:03:58 +0200

    Add files via upload

--- Utumno.tsx
+++ Utumno.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="Utumno" tilewidth="32" tileheight="32" tilecount="6080" columns="64">
- <image source="ProjectUtumno_full.png" width="2048" height="3040"/>
+ <image source="ProjectUtumno_full.png" trans="868686" width="2048" height="3040"/>
  <tile id="0">
   <animation>
    <frame tileid="1615" duration="100"/>
