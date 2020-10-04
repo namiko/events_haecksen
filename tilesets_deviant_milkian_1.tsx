7782f167b6ddca95540ec6e9395c6d010944524b
ghostrich <wiebke.hurrelmann@gmx.de>
2020-10-04 15:33:54 +0200

    Add files via upload

---
+++ tilesets_deviant_milkian_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tilesets_deviant_milkian_1" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="tilesets_deviant_milkian_1.png" trans="000000" width="512" height="512"/>
+</tileset>
