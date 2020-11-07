c35c7a50cf693ed48df4f2ab79416b1933dad944
ghostrich <wiebke.hurrelmann@gmx.de>
2020-11-07 16:14:15 +0100

    Add files via upload

---
+++ hut.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="hut" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="Hut2.png" trans="ffffff" width="32" height="32"/>
+</tileset>
