1c5c47e10913e5e680d3aeac7b72c91316044ae2
namiko <test>
2021-03-21 10:39:08 +0100

    clearance

--- tiles/feuer.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="feuer" tilewidth="32" tileheight="32" tilecount="24" columns="4">
- <image source="feuer.png" trans="ffffff" width="140" height="192"/>
-</tileset>
